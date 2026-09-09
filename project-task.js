2f77a60b0f71f96c804321ad37dade1e0aa2af18
Bradley "KNGDMHTS" Stout <bradmikestout1999@gmail.com>
2026-09-09 18:57:26 -0500

    Update project-task.js
    Added spread syntax to "completedTasks".

--- project-task.js
+++ project-task.js
@@ -73,7 +73,7 @@ const todos = [
 let filteredTodos = todos.filter((taskList) => taskList.completed == false);
 let sortedPriority = todos.sort((a, b) => a.priority - b.priority);
 let sortIncomplete = todos.filter((taskList) => taskList.completed == false).sort((a, b) => a.priority - b.priority);
-let completedTasks = todos.map((taskList) => taskList.completed = true);
+let completedTasks = todos.map((taskList) => ({...taskList, completed: true}));
   
 console.log("Incomplete Tasks:", filteredTodos);
 console.log("Sorted by Priority:", sortedPriority);
